5f6b811bbd2b103ce0915d0eabf71c86ebf5e7ff
Enes <enesak10030@gmail.com>
2023-06-14 18:43:14 +0300

    Added Obstacles
    Added Obstacles

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CollectCoin : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI CoinText;
+    PlayerController playerController;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("Congrats");
+            playerController.runningSpeed = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Carpti");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+    public void AddCoin()
+    {
+        score++;
+        CoinText.text = "Score: " + score.ToString(); 
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {   
     public float runningSpeed;
+    float touchXDelta = 0;
+    float newX = 0;
+    public float xSpeed;
+    public float limitx;
+    public float boostMultiplier; // Hızlandırma katsayısı
+    private bool isBoosting; // Hızlandırma durumu
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +20,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed* Time.deltaTime);
-        transform.position = newPosition;
+        SwipeCheck();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+    {
+        isBoosting = true;
+    }
+    else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+    {
+        isBoosting = false;
+    }
+    }
+
+    private void SwipeCheck()
+    {
+        float currentSpeed = runningSpeed;
+    if (isBoosting)
+    {
+        currentSpeed *= boostMultiplier;
+    }
+
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+        else
+        {
+            touchXDelta = 0;
+        }
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX,-limitx,limitx);
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + currentSpeed * Time.deltaTime);
+        transform.position = newPosition;   
     }
 }
